fdbe8f7ee0ffacea6afbc375211d27f5f177259e
vikas87 <jadhav_vikas87@yahoo.com>
2013-11-21 15:28:43 +0530

    fdszgfdgfd
    bhfcghfdhfr

--- Renvela/ViewController.m
+++ Renvela/ViewController.m
@@ -122,4 +122,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
 @end
